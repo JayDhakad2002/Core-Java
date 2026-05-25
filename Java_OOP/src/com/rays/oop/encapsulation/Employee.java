7f2701702984b5b0068e1c95a6dc1ab4ac450a69
JayDhakad2002 <jaydhakad2002@gmail.com>
2026-05-25 11:54:56 +0530

    done

---
+++ Java_OOP/src/com/rays/oop/encapsulation/Employee.java
@@ -0,0 +1,50 @@
+package com.rays.oop.encapsulation;
+
+public class Employee {
+	
+	private String name;
+	private int id;
+	private int salary;
+	private String workinghours;
+
+	public String getName() {
+		return this.name;
+
+	}
+
+	public void setName1(String name) {
+		this.name = name;
+
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getSalary() {
+		return this.salary;
+
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	public String getWorkinghours() {
+		return this.workinghours;
+		
+	}
+	public void setWorkinghours(String workinghours) {
+		this.workinghours = workinghours;
+	}
+
+	public void setName(String name2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
