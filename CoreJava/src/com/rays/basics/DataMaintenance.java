75fda2b0077193c2b9a645c7e1ac765fd63a6d4c
JayDhakad2002 <jaydhakad2002@gmail.com>
2026-05-25 11:52:32 +0530

    done

---
+++ CoreJava/src/com/rays/basics/DataMaintenance.java
@@ -0,0 +1,21 @@
+package com.rays.basics;
+
+import java.util.Calendar;
+
+public class DataMaintenance {
+	
+	public static void main(String[] args) {
+
+		Calendar c = Calendar.getInstance();
+
+		for (int a = 1; a <= 12; a++) {
+
+			c.add(Calendar.DATE, 30);
+
+			System.out.println(c.getTime());
+		}
+	}
+
+}
+
+
---
+++ CoreJava/src/com/rays/basics/GetDate.java
@@ -0,0 +1,15 @@
+package com.rays.basics;
+
+import java.time.LocalDate;
+
+     public class GetDate {
+	
+     public static void main(String[] args) {
+    	 
+	  LocalDate today = LocalDate.now();
+	  
+	  System.out.println("Current Date is :- " + today);
+	
+   }
+
+}
