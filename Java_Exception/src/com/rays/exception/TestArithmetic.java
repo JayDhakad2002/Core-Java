78bb8823a28fad353d3220afa5a35aa4c697fc1c
JayDhakad2002 <jaydhakad2002@gmail.com>
2026-06-13 11:13:02 +0530

    done

---
+++ Java_Exception/src/com/rays/exception/TestArithmetic.java
@@ -0,0 +1,16 @@
+package com.rays.exception;
+
+public class TestArithmetic {
+	
+	public static void main(String[] args) {
+		
+		int a = 10;
+	    int b = 0;
+		int c = a/b;
+		
+		System.out.println("div" + c);
+		System.out.println("exception aagyi");
+	
+	}
+
+}
