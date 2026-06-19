09bc9171df3e4e918632a8337c3c8d6d9520d4ce
JayDhakad2002 <jaydhakad2002@gmail.com>
2026-06-19 18:40:43 +0530

    done

---
+++ Java_Exception/src/com/rays/exception/TestArithmetic01.java
@@ -0,0 +1,16 @@
+package com.rays.exception;
+
+public class TestArithmetic01 {
+	
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 0;
+		int c = a/b;
+		
+		System.out.println(c);
+		System.out.println("program is finish");
+		
+	}
+
+}
