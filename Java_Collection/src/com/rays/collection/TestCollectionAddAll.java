3b54b319625aa0982b1a7ac15a1ff6d73842122d
JayDhakad2002 <jaydhakad2002@gmail.com>
2026-06-22 18:29:37 +0530

    done

---
+++ Java_Collection/src/com/rays/collection/TestCollectionAddAll.java
@@ -0,0 +1,43 @@
+package com.rays.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class TestCollectionAddAll {
+
+	public static void main(String[] args) {
+
+		Collection c1 = new ArrayList();
+
+		c1.add(45);
+		c1.add("Ram");
+		c1.add('a');
+		c1.add(4.2);
+		c1.add(true);
+
+		System.out.println("c1: " + c1);
+		System.out.println("size of c2: " + c1.size());
+
+		System.out.println("------------");
+
+		Collection c2 = new ArrayList();
+
+		c2.add(78);
+		c2.add("Dadu");
+		c2.add('b');
+
+		System.out.println("c2: " + c2);
+		System.out.println("size of c2: " + c2.size());
+
+		System.out.println("------------");
+
+		c1.addAll(c2);
+		System.out.println("c1 after adding c2: " + c1);
+
+		System.out.println(c1.remove(c2));
+		System.out.println("c1 after remove c2: " + c1);
+		System.out.println(c1.removeAll(c2));
+
+	}
+
+}
